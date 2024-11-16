ed2d0d609c15158a2202be97e604f34c41da21f4
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-11-16 22:33:53 +0900

    Fix: UI 이상하던거 수정

--- netflix-assignment/src/app/(route)/search/_components/SearchedMovie.tsx
+++ netflix-assignment/src/app/(route)/search/_components/SearchedMovie.tsx
@@ -11,32 +11,28 @@ type SearchedProps = {
 };
 
 const MovieCard = ({ movie }: { movie: Movie }) => (
-  <div key={movie.id} className="w-[146px] h-[220px] bg-gray-800">
+  <div key={movie.id} className="flex justify-between items-center h-[76px] mb-[3px] pr-3 bg-[#424242]">
     <img
       src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
       alt={movie.title || 'Title'}
       className="w-[146px] h-[76px] object-cover"
     />
-    <div className="px-2">
-      <p className="text-[14.72px]">{movie.title}</p>
-      <Play />
-    </div>
+    <p className="w-40 text-[14.72px] overflow-hidden whitespace-break-spaces text-left">{movie.title}</p>
+    <Play />
   </div>
 );
 
 export default function SearchedMovie({ query }: SearchedProps) {
   const { data } = useFetchSearchedMovies(query);
 
-  if (!data) return <p>검색된 영화가 없습니다.</p>;
+  if (!data) return
 
   return (
     <section className="w-full mt-4">
       <h2 className="text-[26.75px] font-bold mb-[21px] ml-[10px]">Top Searches</h2>
-      <div className="flex flex-wrap gap-4">
-        {data.map((movie: Movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+      {data.map((movie: Movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
     </section>
   );
 }
